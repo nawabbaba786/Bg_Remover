c2ee19a84fdcfdfea69a89332b483f701e41021f
nawabbaba786 <125024365+nawabbaba786@users.noreply.github.com>
2025-10-05 16:30:58 +0500

    Create Footer.js

---
+++ src/app/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
+      <div className="container mx-auto">
+        <p>&copy; 2025 Pro Image Tools. All Rights Reserved.</p>
+        <div className="mt-2">
+          <a href="#" className="text-gray-400 hover:text-white mx-2">Privacy Policy</a>
+          <a href="#" className="text-gray-400 hover:text-white mx-2">About Us</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
